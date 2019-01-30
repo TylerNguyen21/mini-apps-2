8af2c9beb23157b558fda530f26f3d51f8b7e836
Tyler Nguyen <tydnguyen@ucdavis.edu>
2019-01-30 11:40:34 -0800

    complete installtion of json-server and webpack

--- challenge_1/webpack.config.js
+++ challenge_1/webpack.config.js
@@ -9,13 +9,15 @@ module.exports = {
     path: DIST_DIR
   },
   module: {
-    loaders: {
-      test: /\.jsx?/,
-      include: SRC_DIR,
-      loader: 'babel-loader',
-      query: {
-        presets: ['react', 'es2015']
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
       }
-    }
+    ]
   }
 }
