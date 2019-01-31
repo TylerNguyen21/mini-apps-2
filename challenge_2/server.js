c274c9a3cb4b5060f5bdbf9714b0026c8af3e5b0
Tyler Nguyen <tydnguyen@ucdavis.edu>
2019-01-31 15:21:20 -0800

    set the inital server with express

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,24 @@
+const express = require('express');
+const parser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
 
+const app = express();
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, './client/dist')));
+app.use(parser.json());
+app.use(parser.urlencoded({extended: true}));
+
+
+
+
+
+
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
 
