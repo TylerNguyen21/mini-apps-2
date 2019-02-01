43d287cb28d3078cedcf51c9a1d3069d929da099
Tyler Nguyen <tydnguyen@ucdavis.edu>
2019-01-31 16:50:46 -0800

    finish implementing chartjs and rendered the data

--- challenge_2/client/src/components/App.jsx
+++ challenge_2/client/src/components/App.jsx
@@ -7,26 +7,58 @@ class App extends React.Component {
     super(props)
     this.state = {
       start: '2018-01-01',
-      end: '2018-12-31'
+      end: '2018-12-31',
+      dataPoints: []
     }
   }
 
   componentDidMount() {
     axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${this.state.start}&end=${this.state.end}`)
     .then((resp) => {
-      console.log(resp);
+      let current = this.state.start.slice(5,7);
+      let monthData = [];
+      let accumulator = [];
+      let respData = resp.data.bpi;
+      for (var key in respData) {
+        let newKey = key.slice(5,7);
+        if (current === newKey) {
+          accumulator.push(respData[key]);
+        }
+        if (current !== newKey) {
+          current = newKey;
+          accumulator = accumulator.reduce((x, y) => (x + y)/accumulator.length);
+          monthData.push(accumulator);
+          accumulator = [];
+        }
+      }
+      accumulator = accumulator.reduce((x, y) => (x + y)/accumulator.length);
+      monthData.push(accumulator);
+      return monthData;
+    })
+    .then((finalData) => {
+      this.setState({
+        dataPoints: finalData
+      });
     })
     .catch((error) => {
-      console.log('Error:  ', error)
-    })
+      console.log('Error:  ', error);
+    });
   }
 
   render () {
-    return (
-      <div>
-        <Chart />
-      </div>
-    )
+    if (this.state.dataPoints.length > 0) {
+      return (
+        <div>
+          <Chart data={this.state.dataPoints}/>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          
+        </div>
+      )
+    }
   }
 }
 
--- challenge_2/client/src/components/Chart.jsx
+++ challenge_2/client/src/components/Chart.jsx
@@ -1,8 +1,54 @@
 import React from 'react';
+import { Line } from 'react-chartjs-2';
 
+class Chart extends React.Component {
 
-const Chart = props => {
-
+  constructor(props) {
+    super(props)
+    this.state= {
+      chartData: {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"],
+        datasets: [{
+            label: '# of Votes',
+            data: this.props.data,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+    }
+  }
+  
+  render () {
+    console.log(this.props.data);
+    return (
+      <Line data={this.state.chartData}/>
+    )
+  }
 }
+
 
 export default Chart;
